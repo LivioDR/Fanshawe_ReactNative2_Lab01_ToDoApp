3e535ce4c4790d0e58d1c8668ddb2c7129255223
LivioDR <liviodavidreinoso@yahoo.com.ar>
2024-09-26 15:18:02 -0400

    add placeholder for testing

--- App.js
+++ App.js
@@ -1,11 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import TodoListScreen from './screens/TodoListScreen';
+
+const placeholder = [
+  {
+    id: 1,
+    name: 'Test Item',
+    status: true,
+  },
+  {
+    id: 2,
+    name: 'Test Item 2',
+    status: false,
+  },
+  {
+    id: 3,
+    name: 'Test Item 3',
+    status: true,
+  }
+]
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <TodoListScreen todoItems={placeholder}/>
     </View>
   );
 }
