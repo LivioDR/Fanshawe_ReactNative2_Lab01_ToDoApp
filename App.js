be0f260e4edd250261bb929cbff8d3c1023dd936
LivioDR <liviodavidreinoso@yahoo.com.ar>
2024-09-26 15:55:33 -0400

    created data handling functions (toggle, remove) installed uuid dependency

--- App.js
+++ App.js
@@ -1,29 +1,58 @@
-import { StatusBar } from 'expo-status-bar';
+import uuid from 'react-native-uuid';
 import { StyleSheet, Text, View } from 'react-native';
 import TodoListScreen from './screens/TodoListScreen';
+import { useEffect, useState } from 'react';
 
 const placeholder = [
   {
-    id: 1,
+    id: uuid.v4(),
     name: 'Test Item',
     status: true,
   },
   {
-    id: 2,
+    id: uuid.v4(),
     name: 'Test Item 2',
     status: false,
   },
   {
-    id: 3,
+    id: uuid.v4(),
     name: 'Test Item 3',
     status: true,
   }
 ]
 
 export default function App() {
+
+  const [todoItems, setTodoItems] = useState([])
+
+  useEffect(() => {
+    setTodoItems(placeholder)
+  },[])
+
+  const removeItemById = (idToRemove) => {
+    setTodoItems(prev => {
+      let newList = prev.filter(item => item.id != idToRemove)
+      return newList
+    })
+  }
+
+  const toggleItemById = (idToToggle) => {
+    setTodoItems(prev => {
+      let newList = [...prev]
+      for(let i=0; i<newList.length; i++){
+        if(newList[i].id == idToToggle){
+          newList[i].status = !newList[i].status
+          break
+        }
+      }
+      return newList
+    })
+  }
+
+
   return (
     <View style={styles.container}>
-      <TodoListScreen todoItems={placeholder}/>
+      <TodoListScreen todoItems={todoItems} remove={removeItemById} toggle={toggleItemById}/>
     </View>
   );
 }
